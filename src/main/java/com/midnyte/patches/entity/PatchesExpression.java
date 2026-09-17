a62285fbf98eaa6b0206d3769952328e37357cb5
MidnyteSketch <93507412+MidnyteSketch@users.noreply.github.com>
2026-09-16 22:41:48 -0500

    Add Joy expression for curiosity

--- src/main/java/com/midnyte/patches/entity/PatchesExpression.java
+++ src/main/java/com/midnyte/patches/entity/PatchesExpression.java
@@ -6,24 +6,16 @@ public enum PatchesExpression {
     LAUGH(2),
     MOUTH_OPEN(3),
     HURT(4),
-    RESTING(5);
+    RESTING(5),
+    JOY(6);
 
     private final int id;
 
-    PatchesExpression(int id) {
-        this.id = id;
-    }
-
-    public int id() {
-        return id;
-    }
+    PatchesExpression(int id) { this.id = id; }
+    public int id() { return id; }
 
     public static PatchesExpression fromId(int id) {
-        for (PatchesExpression expression : values()) {
-            if (expression.id == id) {
-                return expression;
-            }
-        }
+        for (PatchesExpression expression : values()) if (expression.id == id) return expression;
         return DEFAULT;
     }
 }
